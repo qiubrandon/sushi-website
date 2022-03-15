d8ed12d5fabf1694310fd6215a7f01600e79c561
qiubrandon <80918433+qiubrandon@users.noreply.github.com>
2022-03-14 21:04:26 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.use("/static", express.static('./static/'))
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req,res) => {
+    res.sendFile('home.html', {root: __dirname })
+    console.log("connected")
+});
+
+
+let port = process.env.PORT || 3000 
+app.listen(port,() => {
+    console.log(`Listening on port ${port}...`)
+})
